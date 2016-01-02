437eb17faa9a3d2895d7b1c54709774fb0aeb79e
Danny Mustafic <damirmustafic@me.com>
2016-01-02 08:20:04 -0800

    AT&T Drive SeedApp for 2016 AT&T Developer Summit Hackathon

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # seed-app
-basic AngularJS seed app for use with AT&amp;T Drive UI Kit
+Basic AngularJS seed app for use with AT&amp;T Drive UI Kit
+
+To use, simply run a web server inside the root directory of this seedapp. 
+For convinence two routes (pages) are created to get you up and running fast. 
+
+For help during the hackathon join (http://attdrive.slack.com)[attdrive.slack.com] #devsummit2016
+
+UI Kit reference site is at: (http://uikit.attdrive.com)[uikit.attdrive.com]
+username: previewLogin
+password: haveFun
+
---
+++ mediaPlayer/controllers/MediaPlayerCtrl.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+
+angular.module('app')
+  .controller('MediaPlayerCtrl', ["$scope", function ($scope) {
+      //$scope.songs = [
+      //    {
+      //        title: 'Higgs Boson Blues',
+      //        artist: 'Nick Cave And The Bad Seeds',
+      //        src: '/mnt/sdcard/Music/8 Higgs Boson Blues.mp3',
+      //        art: 'http://upload.wikimedia.org/wikipedia/en/f/f3/Push_the_Sky_Away.jpg'
+      //    },
+      //    {
+      //        title: 'Mind Your Manners',
+      //        artist: 'Pearl Jam',
+      //        src: '/mnt/sdcard/Music/02 - Mind Your Manners.mp3'
+      //    },
+      //    {
+      //        title: 'History Repeating',
+      //        artist: 'Propellerheads',
+      //        src: '/mnt/sdcard/Music/05. Propellerheads - History Repeating.mp3'
+      //    }
+      //    //,
+      //    //{
+      //    //    title: '',
+      //    //    artist: '',
+      //    //    src: 'audio/unavailable resource.mp3'
+      //    //}
+      //];
+      $scope.songs = [
+        {
+            title: 'Looking At The World', // 5.30 min
+            artist: 'Chick Corea',
+            src: 'http://freedownloads.last.fm/download/6814816/Looking%2BAt%2BThe%2BWorld.mp3',
+            art: 'http://userserve-ak.last.fm/serve/126/90745177.jpg'
+        },
+        {
+            title: 'Sound Of Music', // 4+ min
+            artist: 'Prague Ska Conspiracy',
+            src: 'http://freedownloads.last.fm/download/145187271/Sound%2BOf%2BMusic.mp3',
+            art: 'http://userserve-ak.last.fm/serve/126/10239727.jpg'
+        },
+        {
+            title: 'Roots', // 6.30+ min
+            artist: 'indigo jam unit',
+            src: 'http://freedownloads.last.fm/download/464510093/Roots.mp3'
+        },
+        {
+            title: 'Alice\'s Adventures in Wonderland ', // 6.30+ min
+            artist: 'Lewis Carroll',
+            src: 'http://etc.usf.edu/lit2go/audio/mp3/alices-adventures-in-wonderland-001-chapter-i-down-the-rabbit-hole.1.mp3',
+            art: 'http://etc.usf.edu/lit2go/static/thumbnails/books/1.png'
+        }
+      ];
+  }]);
